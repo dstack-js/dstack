0ae4e6e648ada8fcd3c5f4f1d8f0eead191027d9
Mikhail Marynenko <0x77dev@protonmail.com>
2021-09-20 23:06:26 +0300

    add tests

---
+++ lib/src/index.test.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import { createStack, Stack } from '.'
+
+describe('Stack', () => {
+  let stack: Stack
+
+  beforeAll(async () => {
+    stack = await createStack({ app: 'test' })
+  })
+
+  test('node write', async () => {
+    const { data, on } = await stack.node<string>('hello', 'world')
+    on((data) => expect(data).toBe('world'))
+    expect(data).toBe('world')
+  })
+
+  test('node read', async () => {
+    const { data, on } = await stack.node<string>('hello')
+    on((data) => expect(data).toBe('world'))
+    expect(data).toBe('world')
+  })
+})
