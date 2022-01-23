8bea01b2ed4786365f4661b2ea7b9abecc4b0866
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-01-23 04:24:40 +0200

    chore(ipfs): update default webrtc signaling address

--- packages/ipfs/src/index.ts
+++ packages/ipfs/src/index.ts
@@ -10,8 +10,7 @@ export const create = (options?: IPFS.Options): Promise<IPFS.IPFS> => {
         ...options?.config?.Addresses,
         Swarm: [
           ...(options?.config?.Addresses?.Swarm || [
-            '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
-            '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+            '/dns4/dstack-relay.herokuapp.com/tcp/443/wss/p2p-webrtc-star/'
           ]),
         ],
       }
