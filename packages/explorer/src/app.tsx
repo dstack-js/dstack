1632b9132b43ef06715b33779dbfd679ecc1bb05
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-01-27 16:04:51 +0200

    feat(explorer): basic playground

--- packages/explorer/src/app.tsx
+++ packages/explorer/src/app.tsx
@@ -1,16 +1,27 @@
 import { create } from '@dstack-js/ipfs';
 import { Stack } from '@dstack-js/lib';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const App: React.FunctionComponent<{}> = () => {
+  const [ready, setReady] = useState(false);
+
   const init = async (): Promise<void> => {
     const ipfs = await create();
-    const stack = Stack.create('stack', ipfs);
+    const stack = await Stack.create('stack', ipfs);
+
+    // @ts-expect-error
+    window.stack = stack;
+
+    setReady(true);
   };
 
   React.useEffect(() => {
     init();
   }, []);
 
-  return <h1>Hello</h1>;
+  return ready ? (
+    <pre>use `window.stack` to get started</pre>
+  ) : (
+    <h3>Initializing</h3>
+  );
 };
