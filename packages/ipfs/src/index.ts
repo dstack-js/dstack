ef7d40fae49bf891169d18914ddca5bd35e1dfdb
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-02-02 23:05:38 +0200

    feat(ipfs): remove useless dependencies and configs

--- packages/ipfs/src/index.ts
+++ packages/ipfs/src/index.ts
@@ -1,58 +1,47 @@
-import { create as IPFSCreate, PeerId, CID } from 'ipfs'
-import type { IPFS, Options as IPFSOptions } from 'ipfs-core'
-// @ts-expect-error: no-types
-import WebRTCStar from 'libp2p-webrtc-star'
-// @ts-expect-error: no-types
-import WS from 'libp2p-websockets'
-import { NOISE } from '@chainsafe/libp2p-noise'
-import { listen } from './addresses'
+import { create as IPFSCreate, PeerId, CID } from 'ipfs';
+import type { IPFS, Options as IPFSOptions } from 'ipfs-core';
+import { listen } from './addresses';
 
-export type Options = Omit<IPFSOptions, 'libp2p'>
-
-const create = (options?: Options, wrtc?: any): Promise<IPFS> => {
+const create = (options?: IPFSOptions, wrtc?: any): Promise<IPFS> => {
   return IPFSCreate({
     config: {
       Discovery: {
-        webRTCStar: { Enabled: true }
+        webRTCStar: { Enabled: true },
       },
       Addresses: {
-        Swarm: listen
+        Swarm: listen,
       },
       Bootstrap: [
-        '/dns4/relay.dstack.dev/tcp/443/wss/p2p-webrtc-star/p2p/QmV2uXBKbii29iJKHKVy8sx5m49qdDTBYNybVoa5uLJtrf'
+        '/dns4/relay.dstack.dev/tcp/443/wss/p2p-webrtc-star/p2p/QmV2uXBKbii29iJKHKVy8sx5m49qdDTBYNybVoa5uLJtrf',
       ],
-      ...options?.config
+      ...options?.config,
     },
     ...options,
     relay: {
       enabled: true,
       hop: {
-        enabled: true
-      }
+        enabled: true,
+      },
     },
     libp2p: {
-      modules: {
-        transport: [WS, WebRTCStar],
-        connEncryption: [NOISE]
-      },
       config: {
         peerDiscovery: {
           webRTCStar: {
-            enabled: true
-          }
+            enabled: true,
+          },
         },
         transport: {
           WebRTCStar: {
-            wrtc
-          }
-        }
+            wrtc,
+          },
+        },
       },
       dht: {
         enabled: true,
-        kBucketSize: 20
-      }
-    }
-  })
-}
+        kBucketSize: 20,
+      },
+    },
+  });
+};
 
-export { PeerId, CID, create }
+export { PeerId, CID, create };
