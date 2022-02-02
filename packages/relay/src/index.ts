5e72b51595e591ebc68b882c565139ca5b29e582
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-02-02 22:59:02 +0200

    chore(lint): fix all

--- packages/relay/src/index.ts
+++ packages/relay/src/index.ts
@@ -1,42 +1,46 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { create } from '@dstack-js/ipfs'
-import { Stack } from '@dstack-js/lib'
+import { create } from '@dstack-js/ipfs';
+import { Stack } from '@dstack-js/lib';
 
-const wrtc = require('wrtc')
-const { start } = require('libp2p-webrtc-star-signalling-server')
+const wrtc = require('wrtc');
+const { start } = require('libp2p-webrtc-star-signalling-server');
 
 const run = async () => {
-  if (!process.env['DNS_NAME']) {
-    console.warn('no \'DNS_NAME\' env has been set, running in local mode')
+  if (!process.env.DNS_NAME) {
+    console.warn("no 'DNS_NAME' env has been set, running in local mode");
   }
 
   await start({
-    port: process.env['PORT'] || 9090,
-    host: process.env['HOST'] || '0.0.0.0',
-    metrics: !process.env['DISABLE_METRICS'] && true
-  })
+    port: process.env.PORT || 9090,
+    host: process.env.HOST || '0.0.0.0',
+    metrics: !process.env.DISABLE_METRICS && true,
+  });
 
-  if (process.env['NAMESPACE']) {
-    const ipfs = await create({
-      start: true,
-      ...(process.env['PRIVATE_KEY'] ? { init: { privateKey: process.env['PRIVATE_KEY'] } } : {}),
-      relay: {
-        enabled: true,
-        hop: {
+  if (process.env.NAMESPACE) {
+    const ipfs = await create(
+      {
+        start: true,
+        ...(process.env.PRIVATE_KEY
+          ? { init: { privateKey: process.env.PRIVATE_KEY } }
+          : {}),
+        relay: {
           enabled: true,
-          active: true
+          hop: {
+            enabled: true,
+            active: true,
+          },
         },
-      }
-    }, wrtc)
+      },
+      wrtc
+    );
 
-    await Stack.create(process.env['NAMESPACE'], ipfs)
+    await Stack.create(process.env.NAMESPACE, ipfs);
   }
 
-  console.log("ready")
-}
+  console.log('ready');
+};
 
-run()
-  .catch((error) => {
-    console.error(error)
-    process.exit(-1)
-  })
+run().catch((error) => {
+  console.error(error);
+  process.exit(-1);
+});
