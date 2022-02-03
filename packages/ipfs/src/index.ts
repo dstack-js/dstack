0fdd80e0fce8673bd9dbfc1a9ad47782420f0af6
Mikhailo Marynenko <0x77dev@protonmail.com>
2022-02-03 21:02:21 +0200

    chore(ipfs): format lint

--- packages/ipfs/src/index.ts
+++ packages/ipfs/src/index.ts
@@ -1,47 +1,47 @@
-import { create as IPFSCreate, PeerId, CID } from 'ipfs';
-import type { IPFS, Options as IPFSOptions } from 'ipfs-core';
-import { listen } from './addresses';
+import { create as IPFSCreate, PeerId, CID } from 'ipfs'
+import type { IPFS, Options as IPFSOptions } from 'ipfs-core'
+import { listen } from './addresses'
 
 const create = (options?: IPFSOptions, wrtc?: any): Promise<IPFS> => {
   return IPFSCreate({
     config: {
       Discovery: {
-        webRTCStar: { Enabled: true },
+        webRTCStar: { Enabled: true }
       },
       Addresses: {
-        Swarm: listen,
+        Swarm: listen
       },
       Bootstrap: [
-        '/dns4/relay.dstack.dev/tcp/443/wss/p2p-webrtc-star/p2p/QmV2uXBKbii29iJKHKVy8sx5m49qdDTBYNybVoa5uLJtrf',
+        '/dns4/relay.dstack.dev/tcp/443/wss/p2p-webrtc-star/p2p/QmV2uXBKbii29iJKHKVy8sx5m49qdDTBYNybVoa5uLJtrf'
       ],
-      ...options?.config,
+      ...options?.config
     },
     ...options,
     relay: {
       enabled: true,
       hop: {
-        enabled: true,
-      },
+        enabled: true
+      }
     },
     libp2p: {
       config: {
         peerDiscovery: {
           webRTCStar: {
-            enabled: true,
-          },
+            enabled: true
+          }
         },
         transport: {
           WebRTCStar: {
-            wrtc,
-          },
-        },
+            wrtc
+          }
+        }
       },
       dht: {
         enabled: true,
-        kBucketSize: 20,
-      },
-    },
-  });
-};
+        kBucketSize: 20
+      }
+    }
+  })
+}
 
-export { PeerId, CID, create };
+export { PeerId, CID, create }
